5c0a53671610ff8f4b6a1d1116fdf1d19a74cab2
john-ngo <ngoxd97@gmail.com>
2020-10-27 21:26:15 +0200

    Homework 42

--- selenium_form.js
+++ selenium_form.js
@@ -4,3 +4,30 @@
 // 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
 // 2. Login with invalid creds and check validation error
 // 3. Logout from app and assert you successfully logged out
+
+/// <reference types="cypress" />
+
+it('Task 1', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[type="text"]').type('tomsmith');
+    cy.get('[type="password"]').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.subheader').should('have.text', 'Welcome to the Secure Area. When you are done click logout below.');
+});
+
+it('Task 2', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[type="text"]').type('o');
+    cy.get('[type="password"]').type('o!');
+    cy.get('.radius').click();
+    cy.get('.flash.error').should('have.text', '\n            Your username is invalid!\n            ×\n          ');
+});
+
+it('Task 3', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+    cy.get('[type="text"]').type('tomsmith');
+    cy.get('[type="password"]').type('SuperSecretPassword!');
+    cy.get('.radius').click();
+    cy.get('.button.secondary.radius').click();
+    cy.get('.flash.success').should('have.text', '\n            You logged out of the secure area!\n            ×\n          ');
+});
